9ffa14cdaea962c5231be8062a4ec54ab00c5bef
Utkarsh Shaw <utkarsh.shaw123@gmail.com>
2023-01-02 13:04:23 +0530

    nav-bar

--- educare/educare/settings.py
+++ educare/educare/settings.py
@@ -123,5 +123,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-from decouple import config
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'your_key'
